925c42da69399688f4a6967bb76f268fef8d0131
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-11-12 09:28:19 -0500

    not yet done

--- Die.cs
+++ Die.cs
@@ -6,92 +6,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Die_Class;
-
-public class Die
+namespace Die_Class
 {
-    private int _roll, _randomFace, _frame;
-    private Random _generator;
-    private List<Texture2D>_faces;
-    private Rectangle _location;
-
-    // For for animation
-    float _rotation;
-
-    public Die(List<Texture2D> faces, Rectangle location)
+    public class Die
     {
-        _generator = new Random();
-        _roll = _generator.Next(1, 7);
-        _faces = faces;
-        _location = location;  
-        _frame = 0;
-        _rotation = 0f;
-    }
+        private Random _generator;
+        private int _roll;
+        private List<Texture2D> _faces;
+        private Rectangle _location;
 
-    // Allows the user to create a Die and specify its starting value
-    public Die(List<Texture2D> faces, Rectangle location, int roll)
-    {
-        _generator = new Random();
-        if (roll < 1 || roll > 6)
-            roll = 6;
-        else
-            _roll = roll;
-        _faces = faces;
-        _location = location;
-        _frame = 0;
-        _rotation = 0f;
-    }
-
-    public int Roll
-    {
-        get { return _roll; }
-    }
-
-    public override string ToString()
-    {
-        return _roll + "";
-    }
-
-    public Rectangle Rect
-    {
-        // Fixes offset error made from rotation
-        //get { return new Rectangle(_location.X - _location.Width / 2, _location.Y - _location.Height / 2, _location.Width, _location.Height); }
-        //set { _location = new Rectangle }
-        get { return _location; }
-        set { _location = value; }               
-    }
-
-    public int RollDie()
-    {
-        _roll = _generator.Next(1, 7);
-        _frame = 1;
-        return _roll;
-    }
-
-    public void DrawRoll(SpriteBatch spriteBatch)
-    {
-        if (_frame > 0) // When _frame is > 0, we are animating a roll
+        public Die(List<Texture2D> faces, Rectangle location)
         {
-            _frame++;
-            _rotation += 0.1f;
-            if (_frame % 10 == 0)   // Every 10 frames display a random die face.
+           _generator = new Random();
+            _roll = _generator.Next(1, 7);
+
+            _faces = faces;
+            _location = location;
+            
+        }
+
+        public int Roll 
+        {
+            get { return _roll; }
+            //set { _roll = value; }
+        }
+     
+        public void RollDie()
+        { 
+            _roll = _generator.Next(1, 7);
+        }
+
+        public Die (int roll)
+        {
+            _generator = new Random();
+            if (roll < 1)
             {
-                _randomFace = _generator.Next(_faces.Count);
-                if (_frame == 60)   // After 60 frames, stop animating a roll
-                {
-                    _frame = 0; // Animation is done
-                    _rotation = 0f;
-                }
+                _roll = 1;
             }
-            // Spins and iterates through dies faces to animate roll, this translates the rotated texture to be on the rectangle for drawing
-            spriteBatch.Draw(_faces[_randomFace], new Rectangle(_location.X + _location.Width/2, _location.Y + _location.Height/2, _location.Width, _location.Height), null, Color.White, _rotation, new Vector2(_faces[0].Width/2, _faces[0].Height/2), SpriteEffects.None, 1);
+            else if (roll > 6)
+            {
+                _roll = 6;
+            }
+            else 
+            { 
+                  _roll = roll;
+            }
         }
-        else
-        {
-            // Draws final roll
-            spriteBatch.Draw(_faces[_roll - 1], _location, Color.White);
 
+        public void DrawRoll(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_faces[_roll - 1], _location, Color.White);
         }
+
+
+
 
     }
 }
--- Game1.cs
+++ Game1.cs
@@ -10,14 +10,9 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    KeyboardState keyboardState, prevKeyboardState;
-    MouseState mouseState, prevMouseState;
-
     List<Texture2D> dieTextures;
 
     Die die1;
-
-    List<Die> dice;
 
     public Game1()
     {
@@ -31,14 +26,11 @@ public class Game1 : Game
         // TODO: Add your initialization logic here
 
         dieTextures = new List<Texture2D>();
-        dice = new List<Die>();
+
 
         die1 = new Die(dieTextures, new Rectangle(10, 10, 75, 75));
 
-        for(int i = 0; i < 6; i++)
-        {
-            dice.Add(new Die(dieTextures, new Rectangle(100, i * 75, 75, 75)));
-        }
+
         base.Initialize();
     }
 
@@ -48,43 +40,17 @@ public class Game1 : Game
 
         // TODO: use this.Content to load your game content here
 
-        for (int i = 1; i <= 6; i++)
-        {
-            dieTextures.Add(Content.Load<Texture2D>("white_die_" + i));
-        }
+
     }
 
     protected override void Update(GameTime gameTime)
     {
-        prevMouseState = mouseState;
-        mouseState = Mouse.GetState();
-
-        prevKeyboardState = keyboardState;
-        keyboardState = Keyboard.GetState();
 
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
 
-        if(keyboardState.IsKeyDown(Keys.Space) && prevKeyboardState.IsKeyUp(Keys.Space))
-        {
-            foreach(Die die in dice)
-            {
-                die.RollDie();
-            }
-            die1.RollDie();
-        }
-
-        if(mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
-        {
-            foreach(Die die in dice)
-            {
-                if (die.Rect.Contains(mouseState.Position))
-                {
-                    die.RollDie();
-                }
-            }
-        }
+  
 
         // TODO: Add your update logic here
 
@@ -98,11 +64,7 @@ public class Game1 : Game
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
 
-        die1.DrawRoll(_spriteBatch);
-        foreach(Die die in dice)
-        {
-            die.DrawRoll(_spriteBatch);
-        }
+
 
         _spriteBatch.End();
         base.Draw(gameTime);
